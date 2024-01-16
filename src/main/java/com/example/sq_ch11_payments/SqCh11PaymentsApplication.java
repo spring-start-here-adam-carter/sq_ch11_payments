3112d9f2edb5dcbfd556466feb969f1a3f976fd4
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-15 20:43:19 -0500

    Initial commit

---
+++ src/main/java/com/example/sq_ch11_payments/SqCh11PaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch11_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh11PaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh11PaymentsApplication.class, args);
+	}
+
+}
